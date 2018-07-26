a9e4b876bb3a405349978d2d7dce836f298fbacf
DaMianYang <hugo.dupuyroudel@outlook.com>
2018-07-26 22:48:05 +0200

    init

---
+++ go.sh
@@ -0,0 +1,73 @@
+if [ ! -d ~/.shortcuts ]; then
+	mkdir ~/.shortcuts
+fi
+
+go(){
+		shortcuts=~/.shortcuts/
+		if [ $# -eq 0 ]; then
+				echo "$0 [-r] <shorcut>"
+		elif [ $1 = "-l" ]; then
+				ls -o $shortcuts
+		elif [ $1 = "-r" ]; then
+				unlink $shortcuts$2
+		else
+				previousCD=`pwd`
+				#cd `~/Script/go_shortcut.sh $1`
+				cd $shortcuts$1
+				cd `pwd -P`
+				currentCD=`pwd`
+				cd $previousCD
+				cd $currentCD
+				echo `pwd`
+		fi
+}
+
+goadd(){
+		shortcuts=~/.shortcuts/
+		if [ $# -eq 0 ]; then
+				echo "$0 <folder_to_link> <shortcut>"
+		else
+				# if the first character of the folder to link is "~" or "/"
+				if [ ${1:0:1} = "~" -o ${1:0:1} = "/" ]; then
+						path=$1
+						#ln -s $1 $shortcuts$2
+				else
+						path=`pwd`/$1
+				fi
+#				if [[ "$2" == */* ]]; then
+#						echo "val $2"
+#						arrIN=(${2//\// })
+#						numDir=$((${#arrIN[@]} - 1))
+#						echo $numDir
+#						for i in $(seq 0 $numDir)
+#						do
+#							echo $i
+#							val=${arrIN[$i]}
+#							if [ ! -d $shortcuts$val ]; then
+#								mkdir $shortcuts$val
+#							fi
+#						done
+
+#						oldIFS=$IFS
+#						for i in ${array[@]} do
+#
+#						done
+				#fi
+				ln -s "$path" "$shortcuts$2"
+		fi
+}
+						#IFS='/' read -ra array <<< $2
+						
+
+_go() 
+{
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts=`ls ~/.shortcuts/`
+
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    return 0
+}
+complete -F _go go
---
+++ up.sh
@@ -0,0 +1,29 @@
+up(){
+		cd "$1"
+}
+
+_up() 
+{
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts=`pwd`
+
+	if [[ $cur == /* ]]; then
+		COMPREPLY=()
+		return 0
+	else
+		COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+
+		local escaped_single_qoute="'\''"
+		local tmp=""
+		for entry in ${COMPREPLY[@]}
+		do
+        tmp=$tmp"$entry\ "
+		done
+    COMPREPLY=("${tmp:0:-2}/")
+		return 0
+	fi
+}
+complete -o dirnames -o nospace -F _up up
